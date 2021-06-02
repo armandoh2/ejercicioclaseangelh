81ab8007115a62d36e0fd6caf3ab5d29ad186a90
armandoh2 <71039244+armandoh2@users.noreply.github.com>
2021-06-02 17:38:29 -0500

    Create FuncionesExternas.py

---
+++ clase1/FuncionesExternas.py
@@ -0,0 +1,13 @@
+#Declaracion de funciones
+
+#Funcion sin parametros
+
+def funcion1 ():
+	print('Has llamado a la funcion 1')
+
+#Funcion con parametros y retorno
+
+def funcion2(a,b):
+	#contenido de la funcion
+	c=int(a)+int(b)
+	return c
